5f17506d47b4eb3d9781245987c91a3cb50576c0
Ashley Hoath <ahoath87@gmail.com>
2023-01-03 12:44:20 -0700

    finished getUser with passed test

--- db/users.js
+++ db/users.js
@@ -5,28 +5,57 @@ const client = require("./client");
 // user functions
 async function createUser({ username, password }) {
   try {
-    const { rows: [user] } = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING
     RETURNING username;
-    `, [username, password]);
+    `,
+      [username, password]
+    );
     return user;
   } catch (error) {
     throw error;
   }
-};
+}
 
 async function getUser({ username, password }) {
+  if (!username || !password) {
+    return;
+  }
+  try {
+    const user = await getUserByUsername(username);
 
+    if (password === user.password) {
+      delete user.password;
+      console.log("user", user);
+      return user;
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUserById(userId) {
+async function getUserById(userId) {}
 
-}
-
-async function getUserByUsername(userName) {
-
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT * FROM users
+    WHERE username = $1;
+    `,
+      [username]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
@@ -34,4 +63,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
