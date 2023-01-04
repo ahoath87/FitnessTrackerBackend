0cd44ed73e5d774476074aad23aa6dec0db17a8c
Anthony <anthonypenaloza99@gmail.com>
2023-01-04 09:05:21 -0600

    finished through get all activities

--- db/activities.js
+++ db/activities.js
@@ -2,11 +2,28 @@ const client = require('./client');
 
 // database functions
 async function createActivity({ name, description }) {
-  // return the new activity
+  try {
+    const { rows: [activity] } = await client.query(`
+    INSERT INTO activities(name, description)
+    VALUES ($1, $2)
+    RETURNING *;
+    `, [name, description]);
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getAllActivities() {
-  // select and return an array of all activities
+  try {
+    const { rows } = await client.query(`
+    SELECT *
+    FROM activities;
+    `)
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getActivityById(id) {}
--- db/seedData.js
+++ db/seedData.js
@@ -1,5 +1,6 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
 const { createUser, getUserByUsername, getUser } = require("./users");
+const { createActivity } = require("./activities")
 const client = require("./client");
 
 async function dropTables() {
@@ -7,8 +8,10 @@ async function dropTables() {
     console.log("Dropping All Tables...");
     await client.query(`
     DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS activities;
     `);
     console.log("Finished dropping tables");
+
   } catch (error) {
     console.error("Error dropping tables");
     throw error;
@@ -23,6 +26,12 @@ async function createTables() {
       id SERIAL PRIMARY KEY,
       username VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE activities (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
     );
 
 
