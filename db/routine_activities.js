6412ed02e7e702b4d6618b20822521a963f2bd98
Ashley Hoath <ahoath87@gmail.com>
2023-01-05 10:01:12 -0700

    finished db section of project

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -43,24 +43,81 @@ async function getRoutineActivityById(id) {
 
 async function getRoutineActivitiesByRoutine({ id }) {
   try {
-    const {
-      rows
-    } = await client.query(`
+    const { rows } = await client.query(
+      `
   SELECT *
   FROM routine_activities
   WHERE "routineId" = $1
-  `, [id]);
-  return rows;
+  `,
+      [id]
+    );
+    return rows;
   } catch (error) {
     throw error;
   }
 }
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length > 0) {
+      const {
+        rows: [routineActivity],
+      } = await client.query(
+        `
+    UPDATE routine_activities
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `,
+        Object.values(fields)
+      );
+      return routineActivity;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function destroyRoutineActivity(id) {}
+async function destroyRoutineActivity(id) {
+  try {
+    const {
+      rows: [routineActivity],
+    } = await client.query(
+      `
+    DELETE FROM routine_activities 
+    WHERE id = $1
+    RETURNING *;
+    `,
+      [id]
+    );
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try {
+    const {
+      rows: [editableRoutineActivity],
+    } = await client.query(
+      `
+    SELECT routine_activities.*
+    FROM routine_activities
+    JOIN routines ON routines.id = routine_activities."routineId"
+    WHERE routines."creatorId" = $1 AND routine_activities.id = $2
+    `,
+      [userId, routineActivityId]
+    );
+    console.log("editableRoutineActivity", editableRoutineActivity);
+    return editableRoutineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
   getRoutineActivityById,
